9eae8f6bd13b121d88ce46b9564409caf2263da6
Daniel Dumont <dbldumont11@gmail.com>
2026-06-06 12:24:14 +0100

    Removed all full res images to speed up loading

--- images/manifest.js
+++ images/manifest.js
@@ -1,6 +1,6 @@
 const portfolioManifest = {
 "sport": [],
-"automotive": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+"automotive": [],
 "wildlife": [],
-"other": ["1.jpg", "2.jpg", "3.jpg"]
+"other": []
 };
