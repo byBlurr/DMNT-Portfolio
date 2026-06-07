e2f7efb8c716eb15124b4fcb3d778a6ac266cb30
Daniel Dumont <dbldumont11@gmail.com>
2026-06-07 17:53:22 +0100

    - Added images

--- images/manifest.js
+++ images/manifest.js
@@ -1,6 +1,6 @@
 const portfolioManifest = {
 "sport": [],
-"automotive": [],
-"wildlife": [],
-"other": []
+"automotive": ["1.jpg"],
+"wildlife": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"],
+"other": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"]
 };
